6c716cd4c81cdb0c51e1bef7cd5fdfec3def3792
dacaga-hub <daniel.caravacagarcia@gmail.com>
2026-04-09 20:30:21 +0200

    created class samarretes

--- lib/main.dart
+++ lib/main.dart
@@ -5,3 +5,20 @@ import 'calculadora_samarretes.dart';
 void main() {
   runApp(const SamarretesApp());
 }
+
+class SamarretesApp extends StatelessWidget {
+  const SamarretesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Calculadora de Samarretes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        useMaterial3: true,
+      ),
+      home: const CalculadoraScreen(),
+    );
+  }
+}
